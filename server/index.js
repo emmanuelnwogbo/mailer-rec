13f62da36c1e6aaf4892bdcc45e8d226f950dc41
emmanuelnwogbo <nerdyemmanuel@gmail.com>
2023-01-09 01:14:08 +0100

    init

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://osnoanix.com");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     /*if (req.method === 'OPTIONS') {
       //res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
