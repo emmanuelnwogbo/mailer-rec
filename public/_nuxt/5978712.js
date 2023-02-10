f36f48b9d441523461f3665fca0f163f1c7864ff
emmanuelnwogbo <nerdyemmanuel@gmail.com>
2023-02-10 02:01:20 +0100

    remived url bug

--- public/_nuxt/5978712.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{311:function(n,e,t){"use strict";t.r(e);var l={name:"IndexPage"},o=t(29),component=Object(o.a)(l,(function(){return(0,this._self._c)("Landing")}),[],!1,null,null,null);e.default=component.exports;installComponents(component,{Landing:t(303).default})}}]);
