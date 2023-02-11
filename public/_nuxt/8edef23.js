c7054c2735a03885e284c117ca7fccd012c264e3
emmanuelnwogbo <nerdyemmanuel@gmail.com>
2023-02-11 18:04:39 +0100

    add mobile responsiveness

---
+++ public/_nuxt/8edef23.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{334:function(t,n,e){"use strict";e.r(n);var c={},r=e(29),component=Object(r.a)(c,(function(){this._self._c;return this._m(0)}),[function(){var t=this._self._c;return t("div",[t("div",{staticClass:"header"})])}],!1,null,"dd06273a",null);n.default=component.exports}}]);
